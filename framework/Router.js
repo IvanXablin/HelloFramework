8ed4f154202aed9f61918b7dc7c900c84ac9ebac
Ivan Kondratev <80636346+IvanXablin@users.noreply.github.com>
2022-05-03 21:23:22 +0700

    add framework

---
+++ framework/Router.js
@@ -0,0 +1,35 @@
+module.exports = class Router {
+
+    constructor() {
+        this.endpoints = {};
+    }
+
+    request(method = 'GET', path, handler) {
+        if(!this.endpoints[path]) {
+            this.endpoints[path] = {};
+        }
+
+        const endpoints = this.endpoints[path];
+
+        if(endpoints[method]) {
+            throw new Error(`По адресу ${path} метод ${method} уже существует`);
+        }
+
+        endpoints[method] = handler;
+    }
+
+    get (path, handler) {
+        this.request('GET', path, handler);
+    }
+    post (path, handler) {
+        this.request('POST', path, handler);
+    }
+    put (path, handler) {
+        this.request('PUT', path, handler);
+    }
+    delete (path, handler) {
+        this.request('DELETE', path, handler);
+    }
+
+}
+
